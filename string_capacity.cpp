aed7f916ca93336fb8b58f643d323e558b365765
Nasim <mdnasimhosen333@gmail.com>
2025-05-31 20:53:49 +0600

    refactor: format code for consistency and readability

--- string_capacity.cpp
+++ string_capacity.cpp
@@ -1,9 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     string s = "Hello, World!";
     int size = s.size();
-    
+    int max_size = s.max_size();
+
     return 0;
 }
