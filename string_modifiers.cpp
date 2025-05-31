09a32719da6741df53745d8d75254833ed19138f
Nasim <mdnasimhosen333@gmail.com>
2025-05-31 21:52:46 +0600

    fix: update output content and simplify string assignment in string_modifiers.cpp

--- string_modifiers.cpp
+++ string_modifiers.cpp
@@ -5,11 +5,12 @@ int main()
 {
     string s = "Hello";
     string s2 = "World";
-    s += s2;
-    s.append(s2);
-    s.push_back('!');
-    s += "!";
-    s.pop_back();
+    // s += s2;
+    // s.append(s2);
+    // s.push_back('!');
+    // s += "!";
+    // s.pop_back();
+    s = s2;
     cout << s << endl;
     return 0;
 }
