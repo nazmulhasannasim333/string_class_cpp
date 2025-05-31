9a15915162df168a15d15b9af5e2c0239828d8d1
Nasim <mdnasimhosen333@gmail.com>
2025-05-31 20:55:18 +0600

    style: improve formatting and spacing in string_capacity.cpp

--- string_capacity.cpp
+++ string_capacity.cpp
@@ -1,13 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     string s = "Hello, World!";
     int size = s.size();
     int max_size = s.max_size();
     int capacity = s.capacity();
     s.clear();
     bool empty = s.empty();
-    
+    s.resize(10, 'x'); // Resize
     return 0;
 }
