019457847b77069ceeed18dd034f8045f6637fe4
Nasim <mdnasimhosen333@gmail.com>
2025-05-31 20:22:11 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello, World!";
+    string s2 = " C++20";
+    if (s == s2)
+        cout << "s and s2 are equal" << endl;
+    else
+        cout << "s and s2 are not equal" << endl;
+    return 0;
+}
