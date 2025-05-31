2039a0383d749f49eaa26c41b3e09d3231ab16dc
Nasim <mdnasimhosen333@gmail.com>
2025-05-31 20:54:10 +0600

    style: fix formatting for consistency in string_capacity.cpp

--- string_capacity.cpp
+++ string_capacity.cpp
@@ -1,11 +1,11 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main(){
     string s = "Hello, World!";
     int size = s.size();
     int max_size = s.max_size();
-
+    int capacity = s.capacity();
+    
     return 0;
 }
