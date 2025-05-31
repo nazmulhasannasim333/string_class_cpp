9d1dcd75a06cd18c16c664eb5076dd89cc6ed33c
Nasim <mdnasimhosen333@gmail.com>
2025-05-31 21:56:21 +0600

    fix: uncomment string manipulation operations in string_modifiers.cpp

--- string_modifiers.cpp
+++ string_modifiers.cpp
@@ -5,15 +5,15 @@ int main()
 {
     string s = "Hello BD";
     string s2 = "World";
-    // s += s2;
-    // s.append(s2);
-    // s.push_back('!');
-    // s += "!";
-    // s.pop_back();
-    // s = s2;
-    // s.assign(s2);
-    // s.erase(3, 2);
-    // s.replace(5, 1, "C++20");
+    s += s2;
+    s.append(s2);
+    s.push_back('!');
+    s += "!";
+    s.pop_back();
+    s = s2;
+    s.assign(s2);
+    s.erase(3, 2);
+    s.replace(5, 1, "C++20");
     s.insert(5, "C++20");
     cout << s << endl;
     return 0;
