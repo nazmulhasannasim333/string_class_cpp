719e6f8f268bd3b7158c2be36440e631851c60be
Nasim <mdnasimhosen333@gmail.com>
2025-05-31 21:33:06 +0600

    fix: update output content and clean up string manipulation in string_modifiers.cpp

--- string_modifiers.cpp
+++ string_modifiers.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main()
 {
     string s = "Hello";
-    // string s2 = "World";
-    // s += s2;
-    // s.append(s2);
+    string s2 = "World";
+    s += s2;
+    s.append(s2);
     s.push_back('!');
     s += "!";
+    s.pop_back();
     cout << s << endl;
     return 0;
 }
