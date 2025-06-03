53031f02d8e467f482a0a620b4ae29561b088e24
Nasim <mdnasimhosen333@gmail.com>
2025-06-03 11:35:02 +0600

    cc

---
+++ string_constructor.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello, World!";
+    // string s("Hello, World!");
+    // string s("Hello", 2);
+    // string s = "Hello, World!";
+    // string t(s, 2);
+    cout << s << endl;
+    return 0;
+}
