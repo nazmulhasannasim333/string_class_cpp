4699c98c74c740a47cb3429dd6948c01b348fd6e
Nasim <mdnasimhosen333@gmail.com>
2025-06-03 12:22:13 +0600

    function inside class

---
+++ function_inside_class.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+    string name;
+    int roll;
+    Student(string name, int roll)
+    {
+        this->name = name;
+        this->roll = roll;
+    }
+    void hello()
+    {
+        cout << "Hello, " << name << "! Your roll number is " << roll << "." << endl;
+    }
+};
+
+int main()
+{
+    Student nasim("Nasim", 1);
+    nasim.hello();
+    return 0;
+}
