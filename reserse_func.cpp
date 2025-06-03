14923443084a93719e73ce576089b0e19e98c646
Nasim <mdnasimhosen333@gmail.com>
2025-06-03 11:53:11 +0600

    reverse string

--- reserse_func.cpp
+++ reserse_func.cpp
@@ -1,21 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    int a[n];
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-    }
-    reverse(a, a + n);
-    for(int i = 0; i < n; i++){
-        cout << a[i] << " ";
-    }
+int main()
+{
+    // int n;
+    // cin >> n;
+    // int a[n];
+    // for(int i = 0; i < n; i++){
+    //     cin >> a[i];
+    // }
+    // reverse(a, a + n);
+    // for(int i = 0; i < n; i++){
+    //     cout << a[i] << " ";
+    // }
 
-    // string s;
-    // cin >> s;
-    // reverse(s.begin(), s.end());
-    // cout << s << endl;
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+    cout << s << endl;
     return 0;
 }
