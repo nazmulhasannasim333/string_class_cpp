876a27bd05e9a46d6089975b185ac5ff765e2c69
Nasim <mdnasimhosen333@gmail.com>
2025-06-03 11:36:56 +0600

    cc

--- string_constructor.cpp
+++ string_constructor.cpp
@@ -6,8 +6,9 @@ int main()
     // string s = "Hello, World!";
     // string s("Hello, World!");
     // string s("Hello", 2);
-    string s = "Hello, World!";
-    string t(s, 2);
+    // string s = "Hello, World!";
+    // string t(s, 2);
+    string s(5, 'A');
     cout << s << endl;
     return 0;
 }
