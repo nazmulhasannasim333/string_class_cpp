7612f532857692b9e2608f39fcc65ee848f6c8e7
Nasim <mdnasimhosen333@gmail.com>
2025-06-03 11:35:34 +0600

    cc

--- string_constructor.cpp
+++ string_constructor.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     // string s = "Hello, World!";
-    string s("Hello, World!");
-    // string s("Hello", 2);
+    // string s("Hello, World!");
+    string s("Hello", 2);
     // string s = "Hello, World!";
     // string t(s, 2);
     cout << s << endl;
